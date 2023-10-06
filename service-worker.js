6b9bf23dd43b6605d3b29fda6a79f455e36c27ce
Wilkor <wilkorguitarrista@msn.com>
2023-10-06 15:45:59 -0300

    fix
    Melhorando o front do monitor de sentimento

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a5b5091bb244aea9cf1cd317f5e4d697.js"
+  "./precache-manifest.963fd4e5f20943055b8e858da3196466.js"
 );
 
 self.addEventListener('message', (event) => {
